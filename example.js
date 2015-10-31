9a13da91e310267ac9aaa0f95a6fbba8dea8a5c9
E.Azer Koçulu <azer@kodfabrik.com>
2015-10-31 12:01:21 -0700

    Fix array answers

--- example.js
+++ example.js
@@ -4,7 +4,7 @@ var form = NewForm('~/projects.json', [
   { key: "github", title: "What's your Github handle?", default: process.env.USER, forOnce: true },
   { key: "workspace", title: "Where do you keep your code?", default: process.cwd(), forOnce: true },
   { key: "name", title: "What's your new project name?", expect: { len: [1], name: "Project Name" } },
-  { title: "Starter", desc: "user/repo (Github) or a full Git URL to clone.", default: "bud/react-starter" },
+  { title: "Starter", desc: "user/repo (Github) or a full Git URL to clone.", commaList: true },
   { title: "Github Repo?", default: "{github}/{name}" }
 ]);
 
